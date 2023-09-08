86d15fd890a018ada1be96fdc108ecb65a6ce21a
Rocky <createcreate313@gmail.com>
2023-09-08 20:15:21 +0530

    adding allowed host

--- Invoice2/settings.py
+++ Invoice2/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-=(4#)ea$y_*$h2oql_x@it6#ri&9ceulp2m2((pg!rmhy6jn9x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1','invoice-03sj.onrender.com']
 
 
 # Application definition
