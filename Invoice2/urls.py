33b4f1feb4247975e463b3403f1656a81cf0b304
Rocky <createcreate313@gmail.com>
2023-09-08 15:51:56 +0530

    update urls.py

--- Invoice2/urls.py
+++ Invoice2/urls.py
@@ -16,7 +16,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from new import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.login_view),
+    path('dashboard/',views.dashboard,name="dashboard"),
+    path('logout/',views.view_logout,name="logout"),
+    path('addinvoice/',views.addClient,name='addinvoice'),
+    path('addservice/',views.addService,name='addservice'),
+    path('addcompany/',views.addCompany,name='addcompany'),
+    path('updatecompany/<int:id>/',views.updatecompany,name="updatecompany"),
+    path('deletecompany/<int:id>/',views.DeleteCompany,name="deletecompany"),
+    path('reviewinvoice/',views.ReviewInvoice,name="reviewinvoice"),
+    path('reviewinvoice/<int:pk>/',views.view,name="")
 ]
